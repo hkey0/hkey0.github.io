2df27bccb7eac938b3cca943815ad2ffeb5e0688
hkey <81494947+hkey0@users.noreply.github.com>
2026-03-17 06:53:22 +0300

    games

--- games/_next/static/UnxdjFJe-tTE_uUv9CHz0/_buildManifest.js
+++ games/_next/static/UnxdjFJe-tTE_uUv9CHz0/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/games//_next/:path+",
+        "destination": "/games/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
